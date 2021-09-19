554fdaafa291ee523086d54205d76f626a01360d
Игорь <igorfendrih@mail.ru>
2021-09-19 11:54:34 +0500

    3 попытка

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var pageHeader = document.querySelector('.page-header');
 
 navMain.classList.remove('main-nav--nojs');
+pageHeader.classList.remove('page-header--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
